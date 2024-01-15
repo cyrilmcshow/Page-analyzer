1122fe0943f9661332d1b224f02e9a902abf4234
cyril <sokolovkirill9875@yandex.ru>
2024-01-15 16:25:17 +0300

    update db

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS urls CASCADE;
 CREATE TABLE urls(
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(255) UNIQUE,
-    created_at timestamp
+    name varchar(255),
+    created_at date
     );
