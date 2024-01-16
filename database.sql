e3642091c460cb92b65f408403c8b21e0a35182e
cyril <sokolovkirill9875@yandex.ru>
2024-01-16 12:57:06 +0300

    update

--- database.sql
+++ database.sql
@@ -4,3 +4,14 @@ CREATE TABLE urls(
     name varchar(255),
     created_at date
     );
+
+DROP TABLE IF EXISTS urls CASCADE;
+create table url_checks(
+	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	url_id bigint references urls(id),
+	status_code int,
+	h1 varchar(255),
+	title varchar(255),
+	description varchar(255),
+	created_at date
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -10,14 +10,6 @@ from datetime import datetime
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
-# namme = 'kek'
-# date = datetime.now().date().isoformat()
-# print(type(date))
-# with conn.cursor() as curs:
-#     curs.execute(f'SELECT * FROM urls WHERE name=%s', (namme,))
-#     print(curs.fetchone())
-# 
-
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
@@ -35,19 +27,19 @@ def get_urls():
         if isinstance(url(entered_url), ValidationError) is False:
             parsed_name = urlparse(entered_url)
             normalized_site_name = f'{parsed_name.scheme}://{parsed_name.netloc}'
-            current_date = datetime.now().date()
+            current_date = datetime.now().date().isoformat()
             with conn.cursor() as curs:
-                curs.execute(f'SELECT * FROM urls WHERE name=%s', (normalized_site_name, ))
+                curs.execute(f'SELECT * FROM urls WHERE name=%s', (normalized_site_name,))
                 if curs.fetchone() is None:
                     curs.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s)',
                                  (normalized_site_name, current_date))
                     conn.commit()
                     curs.execute(f'SELECT id FROM urls WHERE name=%s', (normalized_site_name,))
                     id = curs.fetchone()[0]
-                    
+
                     flash('Страница успешно добавлена')
                     return redirect(url_for('get_urls_id', id=id))
-            
+
                 else:
                     curs.execute(f'SELECT id FROM urls WHERE name=%s', (normalized_site_name,))
                     id = curs.fetchone()[0]
@@ -62,22 +54,43 @@ def get_urls():
 
             return render_template('index.html', messages=messages)
     else:
-        
         with conn.cursor() as curs:
-            curs.execute(f'SELECT * FROM urls')
-            rows = curs.fetchall()[::-1]
+            curs.execute(f'SELECT DISTINCT urls.id, name, url_checks.created_at FROM urls JOIN url_checks ON urls.id=url_checks.url_id ORDER BY urls.id DESC')
+            rows = curs.fetchall()
         return render_template('urls.html', rows=rows)
-
-    
 
 
 @app.route('/urls/<int:id>')
 def get_urls_id(id):
     with conn.cursor() as curs:
-        curs.execute(f'SELECT * FROM urls where id=%s', (id,))
-        row = curs.fetchone()
-    messages = get_flashed_messages()
-    name = row[1]
-    created_at = row[2]
-    
-    return render_template('definite_url.html', messages=messages, id=id, name=name, created_at=created_at)
+        curs.execute(f'SELECT * from url_checks where url_id=%s', (id,))
+        rows_from_urls_checks = curs.fetchall()
+        if rows_from_urls_checks is None:
+            curs.execute(f'SELECT * FROM urls where id=%s', (id,))
+            select_from_urls_table = curs.fetchone()
+            messages = get_flashed_messages()
+            name = select_from_urls_table[1]
+            urls_created_at = select_from_urls_table[2]
+            return render_template('definite_url.html', messages=messages, id=id, name=name,
+                                   urls_created_at=urls_created_at)
+
+        else:
+            curs.execute(f'SELECT * FROM urls where id=%s', (id,))
+            select_from_urls_table = curs.fetchone()
+            name = select_from_urls_table[1]
+            urls_created_at = select_from_urls_table[2]
+            flash('Страница успешно проверена')
+            messages = get_flashed_messages()
+
+            return render_template('definite_url.html', messages=messages, id=id, name=name,
+                                   urls_created_at=urls_created_at, rows=rows_from_urls_checks)
+
+
+@app.post('/urls/<int:id>/checks')
+def run_checks(id):
+    current_date = datetime.now().date().isoformat()
+    with conn.cursor() as curs:
+        curs.execute(f'INSERT INTO url_checks(url_id, created_at) VALUES (%s, %s)', (id, current_date))
+        conn.commit()
+
+    return redirect(url_for('get_urls_id', id=id))
