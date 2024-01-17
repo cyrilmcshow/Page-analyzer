3d4ccfedeede89d071eab98c0a7a3a7d2d1fa1ca
cyril <sokolovkirill9875@yandex.ru>
2024-01-17 22:13:55 +0300

    update

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -7,6 +7,7 @@ from urllib.parse import urlparse
 from validators.url import url
 from validators import ValidationError
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -46,20 +47,24 @@ def get_urls():
                     id = curs.fetchone()[0]
                     flash('Страница уже существует')
                     return redirect(url_for('get_urls_id', id=id))
-            flash('Страница успешно добавлена')
+
 
 
         else:
-            flash('Неккоректный URL')
+            if len(url) > 255:
+                flash('URL превышает 255 символов')
+            else:
+                flash('Неккоректный URL')
             messages = get_flashed_messages()
 
             return render_template('index.html', messages=messages)
     else:
         with conn.cursor() as curs:
-            curs.execute(f'SELECT DISTINCT ON (urls.id) urls.id, name, url_checks.created_at, url_checks.status_code, url_checks.id AS url_checks_id FROM urls JOIN url_checks '
+            curs.execute(f'SELECT DISTINCT ON (urls.id) urls.id, name, url_checks.created_at, url_checks.status_code, '
+                         f'url_checks.id AS url_checks_id FROM urls JOIN url_checks '
                          f'ON urls.id=url_checks.url_id ORDER BY urls.id DESC, url_checks_id DESC')
             rows = curs.fetchall()
-            
+
         return render_template('urls.html', rows=rows)
 
 
@@ -82,28 +87,48 @@ def get_urls_id(id):
             select_from_urls_table = curs.fetchone()
             name = select_from_urls_table[1]
             urls_created_at = select_from_urls_table[2]
-            flash('Страница успешно проверена')
-            messages = get_flashed_messages()
 
-            return render_template('definite_url.html', messages=messages, id=id, name=name,
+            flash('Страница успешно проверена', 'verification')
+            messages_about_verification = get_flashed_messages(with_categories=True, category_filter=['verification'])
+
+            return render_template('definite_url.html',
+                                   messages_about_verification=messages_about_verification, id=id, name=name,
                                    urls_created_at=urls_created_at, rows=rows_from_urls_checks)
 
 
 @app.post('/urls/<int:id>/checks')
 def run_checks(id):
     current_date = datetime.now().date().isoformat()
-    status_code = None
+    status_code = ''
+    title = ''
+    h1 = ''
+    description_content = ''
     try:
         with conn.cursor() as curs:
             curs.execute(f'SELECT name FROM urls WHERE id=%s', (id,))
             site_name = curs.fetchone()[0]
         r = requests.get(site_name)
         status_code = r.status_code
+        text = r.text
+        soup = BeautifulSoup(text, 'html.parser')
+        try:
+            title = soup.title.string
+        except Exception:
+            print('something wrong')
+        try:
+            h1 = soup.h1.string
+        except Exception:
+            print('something wrong')
+        try:
+            description_content = soup.find('meta', attrs={'name': 'description'})['content']
+        except Exception:
+            print('something wrong')
     except Exception:
-        print('something wrong, boy')
+        flash('Произошла ошибка при проверке')
     with conn.cursor() as curs:
-        curs.execute(f'INSERT INTO url_checks(url_id, created_at, status_code) VALUES (%s, %s, %s)', 
-                     (id, current_date, status_code))
+        curs.execute(
+            f'INSERT INTO url_checks(url_id, created_at, status_code, title, h1, description) VALUES (%s, %s, %s, %s, %s, %s)',
+            (id, current_date, status_code, title, h1, description_content))
         conn.commit()
 
     return redirect(url_for('get_urls_id', id=id))
