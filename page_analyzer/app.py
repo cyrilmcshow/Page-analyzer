046e788e59f6410bc30263e73777d2b209de700e
cyril <sokolovkirill9875@yandex.ru>
2024-01-23 22:38:02 +0300

    update

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -24,6 +24,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 
 
 @app.route('/', methods=['GET', 'POST'])
